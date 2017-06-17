d48cb68693080fc2d4c72a31e3bdceaeed97d249
tsuyoshiwada <v23.b.wada@gmail.com>
2017-06-17 15:05:07 +0900

    Updates

---
+++ _next/b7566f9c-1260-4aed-9db4-bd07b972139f/page/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/', function() {
+            var comp = module.exports=webpackJsonp([2],[],[311]);
+            return { page: comp.default }
+          })
+        
---
+++ _next/b7566f9c-1260-4aed-9db4-bd07b972139f/page/test/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/test', function() {
+            var comp = module.exports=webpackJsonp([3],[],[312]);
+            return { page: comp.default }
+          })
+        
